f52c26190335fa2d84b67b57925922a3141f092b
Nagarjuna <nagarjunareddy263@gmail.com>
2022-11-22 20:11:22 +0530

    first commit

---
+++ app4/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def nagarjuna(request):
+    return render(request,'nagarjuna.html')
+    
--- project4/urls.py
+++ project4/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app4.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('nagarjuna/',nagarjuna,name='nagarjuna'),
 ]
